49615abe8fa1459d4f8b2cf0ff57dfedca317810
Paco Boga <javier.boga.rioja@gmail.com>
2024-04-17 11:44:06 +0200

    Hasta servicio de grafica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Color, ScaleType} from '@swimlane/ngx-charts';
+import { FaultsService } from './services/faults.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'grafica';
-}
+
+
+  constructor(private faultsService: FaultsService) {
+     //Object.assign(this, { single })
+  }
+
+  //metodo para obtener la data de la grafica
+  //desde el servicio
+  get single(){
+    return this.faultsService.faultsData;
+  }
+
+  onRandomData(){
+    this.faultsService.randomData();
+  }
+
+  view: [number,number] = [940, 200];
+  // options grafica
+   animations = true;
+   showXAxis = true;
+   showYAxis = true;
+   gradient = false;
+   showLegend = false;
+   showXAxisLabel = true;
+   xAxisLabel = 'Salidas Nivel AF';
+   showYAxisLabel = true;
+   yAxisLabel = 'Fallos Stop Devices';
+   yMax =50;
+   padding=1;
+   showGridLines = true;
+   schemeType = ScaleType.Linear;
+   colorScheme: Color = {
+       //domain: ['#00FF00','#00AA00', '#00DD00','#FF0000'],
+       domain: ['#00FF00','#FF0000'],
+       group: ScaleType.Linear,
+       selectable: true, // => boolean (true / false)
+       name: 'Customer Usage',
+   };
+
+
+   onSelect(event: any) {
+     console.log(event);
+   }
+ }
